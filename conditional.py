5f3d7616e1baafc52774de82d6825c147e66227f
Mr Peter Nwobodo <104633760+McBOGman@users.noreply.github.com>
2022-10-18 15:26:06 +0100

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,10 @@
+name = input("what is your name?\n")
+age = input("How old are you?\n")
+Height = input("What's your height\n")
+first_name = "peter"
+last_name = "nwobodo"
+full_name = (first_name + " " + last_name)
+if name == "PETER NWOBODO":
+    print(full_name.upper())   
+    print("welcome to my world")
+else: print("Access denied")
